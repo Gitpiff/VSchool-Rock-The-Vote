2e030d4191e168a762ff9875845281fabd43450a
Victor <viknavarro@gmail.com>
2022-08-16 12:09:36 -0500

    Update Auth.js

---
+++ client/src/components/Auth.js
@@ -0,0 +1,64 @@
+import React, { useState, useContext } from "react"
+import AuthForm from "./AuthForm"
+import { UserContext } from "../context/UserProvider"
+
+const initInputs= { username: "", password: "" }
+
+export default function Auth(){
+    const [inputs, setInputs] = useState(initInputs)
+    const [toggle, setToggle] = useState(false)
+
+    const { signup, login, errMsg, resetAuthErr } = useContext(UserContext)
+
+    function handleChange(e){
+        const {name, value} = e.target
+        setInputs(prevInputs => ({
+            ...prevInputs,
+            [name]: value
+        }))
+    }
+
+    function handleSignup(e){
+        e.preventDefault()
+        signup(inputs)
+    }
+
+    function handleLogin(e){
+        e.preventDefault()
+        login(inputs)
+    }
+
+    function toogleForm(){
+        setToggle(prev => !prev)
+        resetAuthErr()
+    }
+
+    return (
+        <div className="auth-container">
+            <h1>Rock The Vote App</h1>
+            { !toogle ?
+                <>
+                    <AuthForm
+                        handleChange={handleChange}
+                        handleSubmit={handleSignup}
+                        inputs={inputs}
+                        btnText="Sign Up"
+                        errMsg={errMsg}
+                    />
+                    <p onClick={toogleForm}>Already a Member?</p>
+                </>
+                :
+                <>
+                    <AuthForm
+                        handleChange={handleChange}
+                        handleSubmit={handleSignup}
+                        inputs={inputs}
+                        btnText="Login"
+                        errMsg={errMsg}
+                    />
+                    <p onClick={toogleForm}>Not a Member?</p>
+                </>
+            }
+        </div>
+    )
+}
