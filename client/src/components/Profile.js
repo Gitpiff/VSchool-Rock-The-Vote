67891bf92ac99aaa15990f98bff5d3617b8fd868
Victor <viknavarro@gmail.com>
2022-08-17 10:35:58 -0500

    Update Profile.js

---
+++ client/src/components/Profile.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react"
+import IssueForm from "./IssueForm"
+import IssueList from "./IssueList"
+import { UserContext } from "../context/UserProvider"
+
+export default function Profile(){
+    const {
+        user:{
+            username
+        },
+        addIssue,
+        issues
+    } = useContext(UserContext)
+
+    return(
+        <div className="profile">
+            <h1>Welcome {username}</h1>
+            <h3>Add a Political Issue</h3>
+            <IssueForm addIssue={addIssue} />
+            <h3>Your Posts</h3>
+            <IssueList issues={issues} />
+        </div>
+    )
+}
