2f18eeff2987e131b3e5224185f2408d2c56502d
Victor <viknavarro@gmail.com>
2022-08-17 10:56:14 -0500

    updated User Provider

---
+++ client/src/context/UserProvider.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import axios from "axios";
+export const UserContext = React.createContext()
+
+//create a new configurable version of axios
+const userAxios = axios.create()
+
+userAxios.interceptors.request.use(config => {
+    const token = localStorage.getItem("token")
+    config.headers.Authorization = `Bearer ${token}`
+    return config
+})
+
+export default function UserProvider(props){
+    const initState = {
+        //before setting the user to an empty object, let's check if the user already exists in the local storage, since we stringify it before we need to parse it back to an object, if it's not found in local storage, then we can set it as an empty object
+        user: JSON.parse(localStorage.getItem("user") || {}),
+        //before setting the token to an empty string, let's check if the token already exists in the local storage, otherwise set it as an empty string
+        token: localStorage.getItem("token") || "",
+        issues: [],
+        errMsg: ""
+    }
+    const [userState, setUserState] = useState(initState)
+
+    //Signup function, it takes the credentials as arguments
+    function signUp(credentials){
+        axios.post("/auth/signup", credentials)
+            .then(res => {
+                const { user, token } = res.data    //gets user and token from response
+                //to prevent losing data when we refresh the page, we need to save the user and token into local storage, when storing complex data types like objects or arrays we need to turn them into JSON strings
+                localStorage.setItem("token", token)
+                localStorage.setItem("user", JSON.stringify(user))
+                setUserState(prevUserState => ({
+                    ...prevUserState,
+                    user,
+                    token
+                }))
+            })
+            .catch(err => handleAuthErr(err.response.data.errMsg))
+    }
+
+    //Login function, also takes the credentials as arguments
+    function login(credentials){
+        axios
+    }
+}
