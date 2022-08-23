70c03a0418b63a6618bca1189ad49fe7d5628788
Victor <viknavarro@gmail.com>
2022-08-23 13:07:37 -0500

    changed name from todo to issue

--- client/src/components/IssueList.js
+++ client/src/components/IssueList.js
@@ -2,7 +2,7 @@ import React from "react"
 import Issue from "./Issue"
 
 
-export default function IssueForm(props){
+export default function IssueList(props){
     const { issues } = props
 
     return(
